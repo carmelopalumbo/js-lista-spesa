033ecf47bb7138b8a49fab280ee7bd285f6f8a14
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-18 14:03:47 +0200

    add-for-version

--- for-version/js/script.js
+++ for-version/js/script.js
@@ -1,1 +1,21 @@
 // js
+
+const listaSpesa = [
+    'Pane',
+    'Pasta',
+    'Acqua',
+    'Mozzarelle',
+    'Biscotti',
+    'Coca Cola',
+    'Prosciutto',
+    'Piatti',
+    'Tovaglioli',
+    'Detersivo'
+];
+
+let outputList = document.getElementById('lista');
+
+for(i = 0; i < listaSpesa.length; i++){
+    outputList.innerHTML += `<li> ${listaSpesa[i]} </li>`;
+}
+
