e7254513e5226b1adad0d921372e8a8644436924
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-18 14:07:25 +0200

    add-while-version

--- while-version/js/script.js
+++ while-version/js/script.js
@@ -1,1 +1,23 @@
 // js
+
+const listaSpesa = [
+    'Pane',
+    'Pasta',
+    'Acqua',
+    'Mozzarelle',
+    'Biscotti',
+    'Coca Cola',
+    'Prosciutto',
+    'Piatti',
+    'Tovaglioli',
+    'Detersivo'
+];
+
+let outputList = document.getElementById('lista');
+let c = 0;
+
+while(c < listaSpesa.length){
+    outputList.innerHTML += `<li> ${listaSpesa[c]} </li>`;
+    c++;
+}
+
